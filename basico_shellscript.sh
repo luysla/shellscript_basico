32734ef46bd81a7e06578910605904dc0e92e3a5
luysla <dyana.tavares1@gmail.com>
2018-07-20 22:01:05 -0300

    Organizando o código

--- basico_shellscript.sh
+++ basico_shellscript.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
-
 ###################################
 #                                 #
 #   APRENDENDO COMANDOS BÁSICOS   #
-#		  DO SHELLSCRIPT       	  #
+#		  DO SHELL SCRIPT         #
 #  		                          #
 #  Created by: Luysla Dyana       #
 #                                 #
 ###################################                           
 
-
+#Manipulando strings
 var=helloworld
 
-echo "Substituição:" ${var/world/WORLD} - "Numero de caracteres:"${#var}, "Retirar caracteres do início:" ${var#hello}, "Retirar caracteres do final:" ${var%world}
+echo "Substituição:" ${var/world/WORLD} #Substituindo letras minúsculas por maiuscúlas  
+echo "Número de caracteres na variável:" ${#var}
+echo "Retirar caracteres do início:" ${var#hello}
+echo "Retirar caracteres do final:" ${var%world}
 
-#Execução de comandos
+#Execução de comandos - um após o outro
+echo "Irei executar um comando de listar após esse print" ; ls
 
-#echo "Criar arquivos 1 e 2"; touch arquivo1 arquivo2 ; ls arquivo*
-
-#Filtrar nomes de arquivos com o grep
+#Listar e filtrar arquivos com o grep
 ls | grep arquivo
 
-#Executando um segundo comando caso o primeiro seja executado 
-#ping -c 10 google.com && echo "Conectado!"
+#Executando um segundo comando, caso o primeiro seja executado 
+ping -c 5 google.com && echo "Conectado!"
 
-#Executando um segundo comando caso o primeiro não seja executado
-cat naoexiste.txt || echo "ERRO!" 
+#Executando um segundo comando, caso o primeiro não seja executado
+cat naoexiste.txt || echo "Erro! Arquivo não encontrado" 
 
 #Here documents
-#cat << fim
-#    Argumento: $1
-#    Argumento: $2
-#    Argumento: $3
-#fim
+cat << fim
+    Argumento: $1
+    Argumento: $2
+fim
 
 #Operadores aritméticos
 num1=10
 num2=20
-echo "Soma:" soma=`expr $num1 + $num2`, "Subtração:"`expr $num1 - $num2`, "Multiplicação:"`expr $num1 \* $num2`, "Divisão:"`expr $num1 / $num2`, "Módulo:" `expr $num1 % $num2`  
+
+echo "Soma:" `expr $num1 + $num2`
+echo "Subtração:"`expr $num1 - $num2`
+echo "Multiplicação:"`expr $num1 \* $num2`
+echo "Divisão:"`expr $num1 / $num2`
+echo "Módulo:" `expr $num1 % $num2`  
+
 
 
 
